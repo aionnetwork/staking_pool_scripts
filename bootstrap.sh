3f67d28a1743bca363fd2cd696b47dfc3cfa1324
Jeff Disher <jeffdisher@nuco.io>
2019-07-19 01:59:03 -0400

    Updated to working registry and added more verifications

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,8 +15,10 @@ function require_success()
 function verify_state()
 {
 	address="$1"
-	expected="$2"
-	payload={"jsonrpc":"2.0","method":"eth_call","params":[{"to":"$address"}],"id":1}
+	data="$2"
+	expected="$3"
+
+	payload={"jsonrpc":"2.0","method":"eth_call","params":[{"to":"$address","data":"$data"}],"id":1}
 	response=`curl -s -X POST --data "$payload" 127.0.0.1:8545`
 	if [ "$expected" != "$response" ]
 	then
@@ -58,6 +60,11 @@ do
 	require_success $?
 done
 echo "Deployed to address: \"$address\""
+if [ "0xa056337bb14e818f3f53e13ab0d93b6539aa570cba91ce65c716058241989be9" != "$address" ]
+then
+	echo "Address was incorrect:  Expected 0xa056337bb14e818f3f53e13ab0d93b6539aa570cba91ce65c716058241989be9"
+	exirt 1
+fi
 
 echo "Sending registration call..."
 receipt=`./rpc.sh --call "$PRIVATE_KEY" "01" "a056337bb14e818f3f53e13ab0d93b6539aa570cba91ce65c716058241989be9" "210008726567697374657222a02df9004be3c4a20aeb50c459212412b1d0a58da3e1ac70ba74dde6b4accf4b" "00"`
@@ -76,5 +83,8 @@ require_success $?
 echo "Transaction returned receipt: \"$receipt\".  Waiting for transaction to complete..."
 wait_for_receipt "$receipt"
 echo "Transaction completed"
+
+echo "Verifying that vote was registered...
+verify_state "0xa056337bb14e818f3f53e13ab0d93b6539aa570cba91ce65c716058241989be9" "0x210007676574566f746522a02df9004be3c4a20aeb50c459212412b1d0a58da3e1ac70ba74dde6b4accf4b" '{"result":"0x060000100000000000","id":1,"jsonrpc":"2.0"}'
 
 echo "BOOTSTRAP COMPLETE"
